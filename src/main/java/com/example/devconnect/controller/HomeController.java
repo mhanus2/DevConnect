46e7fa2353d908f3305c7337ec595293978b3f67
mhanus2 <mhmartinhanus@gmail.com>
2024-04-24 13:00:29 +0200

    Added: Home page

--- src/main/java/com/example/devconnect/controller/HomeController.java
+++ src/main/java/com/example/devconnect/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.example.devconnect.controller;
 
+import com.example.devconnect.model.Project;
+import com.example.devconnect.service.ProjectService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final ProjectService projectService;
+
+    public HomeController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
     @GetMapping("/")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+    public String sayHello(Model model) {
+        List<Project> projects = projectService.getAllProjects();
+        model.addAttribute("projects", projects);
         return "home";
     }
 }
